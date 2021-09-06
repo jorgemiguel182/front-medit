3d428bfb36985f7f4504e34538f8bab23d57ea66
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-09-06 11:28:11 -0300

    Email view

---
+++ src/pages/pacient/index.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
+
+import {
+  Box,
+  Container,
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  Button,
+  TextField
+} from '@material-ui/core';
+
+const Pacient = () => {
+
+  const [values, setValues] = useState({
+    email: 'demo@devias.io',
+  });
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+  return (
+    <>
+      <Helmet>
+        <title>Email | Material Kit</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: 'background.default',
+          minHeight: '100%',
+          py: 3
+        }}
+      >
+        <Container maxWidth="lg">
+          <Grid
+            container
+            spacing={3}
+          >
+            <Grid item lg={12} md={12} xs={12}>
+              <form autoComplete="off" noValidate >
+                <Card>
+                  <CardHeader
+                    // subheader="The information can be edited"
+                    title="Email"
+                  />
+                  <Divider />
+                  <CardContent>
+                    <Grid container spacing={3}>
+                      <Grid item md={6} xs={12}>
+                        <TextField
+                          fullWidth
+                          label="Email Address"
+                          name="email"
+                          onChange={handleChange}
+                          required
+                          value={values.email}
+                          variant="outlined"
+                        />
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                  <Divider />
+                  <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 2 }}>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                    >
+                      Enviar
+                    </Button>
+                  </Box>
+                </Card>
+              </form>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  )
+};
+
+export default Pacient;
